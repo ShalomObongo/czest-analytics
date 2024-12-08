4775e77c3f834a29b8fcfd480e816be0c152c71b
Shalom Obongo <shalom.obongo@strathmore.edu>
2024-12-08 22:14:08 +0300

    Deploy fix 1

--- src/app/(dashboard)/dashboard/stores/[storeId]/page.tsx
+++ src/app/(dashboard)/dashboard/stores/[storeId]/page.tsx
@@ -6,6 +6,7 @@ interface StorePageProps {
   params: {
     storeId: string
   }
+  searchParams: { [key: string]: string | string[] | undefined }
 }
 
 async function getStoreData(storeId: string) {
